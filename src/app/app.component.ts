b796cad9ae3c8c3bfecff20e08307cf6c1b87480
Ferid Tola <tolferid@outlook.de>
2021-04-20 17:01:33 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { ViewContainerRef } from '@angular/core';
+import { ComponentFactoryResolver } from '@angular/core';
 import { Component } from '@angular/core';
 
 //Imports für Google Analytics
@@ -19,7 +21,14 @@ export class AppComponent {
   title = 'feridtola';
 
   //begin google analytics
-  constructor(private router: Router) {
+  constructor(
+    
+    private router: Router,
+    //Lazy Loading ViewContainRef & ComponentFactoryResolver
+    private viewContainerRef: ViewContainerRef,
+    private cfr: ComponentFactoryResolver
+    
+    ) {
     this.router.events.pipe(
       filter((event): event is NavigationEnd => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
@@ -32,5 +41,15 @@ export class AppComponent {
   }
 
   //ende google analytics
+
+
+  //Lazy Loading
+  async getLazyPhotos() {
+    this.viewContainerRef.clear();
+    const { HomeComponent } = await import('./home/home.component');
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(HomeComponent)
+    );
+  }
 
 }
